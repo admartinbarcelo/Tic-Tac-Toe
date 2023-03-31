fc812789f78ba4dafdd86ebf043af67c13788dac
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-03-31 23:00:37 +0200

    little bit more

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -2,7 +2,7 @@ import { Square } from './Square.jsx'
 export function WinnerModal ({ winner, resetGame}) {
 
     if (winner === null) return null;
-    const winnerText = winner === false ? 'Tie' : 'Winner:'
+    const winnerText = winner === false ? 'Tiee' : 'Winner:'
     return(
         <section className="winner">
             <div className="text">
